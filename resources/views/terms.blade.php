bc8c4e665b27ef2cf368b4c30d962c8b576c9ac5
Niamh <niamh.linguist@gmail.com>
2024-05-23 15:14:29 +0300

    reusable layouts

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.layout')
+
+@section('content')
+<div>
+    <h1>Terms of Service</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non enim praesent elementum facilisis leo. Diam quam nulla porttitor massa. Et odio pellentesque diam volutpat commodo sed egestas egestas. Pulvinar neque laoreet suspendisse interdum consectetur libero. Ultricies mi eget mauris pharetra et ultrices neque. Nec nam aliquam sem et. Justo donec enim diam vulputate ut pharetra sit amet aliquam. Tristique magna sit amet purus. Mauris cursus mattis molestie a iaculis at erat pellentesque. Risus ultricies tristique nulla aliquet enim tortor at. Sed risus ultricies tristique nulla aliquet enim tortor at.
+    </p>
+    <p>
+        Hac habitasse platea dictumst quisque sagittis purus sit amet. Dolor sit amet consectetur adipiscing elit. Vestibulum lorem sed risus ultricies tristique nulla. Elementum curabitur vitae nunc sed velit dignissim sodales. Mattis molestie a iaculis at erat pellentesque. Ac turpis egestas integer eget aliquet. Ullamcorper malesuada proin libero nunc consequat. Bibendum at varius vel pharetra vel turpis nunc eget lorem. In nisl nisi scelerisque eu ultrices vitae. Purus ut faucibus pulvinar elementum integer enim neque volutpat ac.
+    </p>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [DashboardController::class, 'index']);
 
 Route::get('/profile', [ProfileController::class, 'index']);
+
+Route::get('/terms', function () {
+    return view('terms');
+});
